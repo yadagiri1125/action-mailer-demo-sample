b9d92c2677d9206c45c73a171407aa963c57507d
Yadagiri Tandu <yadagiri1125@gmail.com>
2014-01-01 21:15:35 +0530

    action mailer sample

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MailitY::Application.config.secret_key_base = '1e982b307df90c4770b867ce661efbd5c2d2097619c571a6a2302aab55a89c50f9a567bc2543c08a436186961c8491598a0cb2dcc4c3166ffff57c56c595a829'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MailitY::Application.config.session_store :cookie_store, key: '_mailit_y_session'
